3286b69392fac48fad472488bda79b48c11ac788
FlowerMr <mrgolkar94@gmail.com>
2024-12-02 20:08:26 +0330

    second Commit

---
+++ app/src/main/java/com/littlelemon/weatherappk/ui/screen/components/WeaklyForcast.kt
@@ -0,0 +1,8 @@
+package com.littlelemon.weatherappk.ui.screen.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WeaklyForcast(){
+
+}
--- app/src/main/java/com/littlelemon/weatherappk/ui/screen/weatherScreen.kt
+++ app/src/main/java/com/littlelemon/weatherappk/ui/screen/weatherScreen.kt
@@ -1,13 +1,17 @@
 package com.littlelemon.weatherappk.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.littlelemon.weatherappk.ui.components.ActionBar
+import com.littlelemon.weatherappk.ui.screen.components.ActionBar
+import com.littlelemon.weatherappk.ui.screen.components.AirQuality
+import com.littlelemon.weatherappk.ui.screen.components.DailyForcast
 import com.littlelemon.weatherappk.ui.theme.ColorBackground
 
 @Composable
@@ -24,6 +28,10 @@ fun WeatherScreen(){
             .padding(horizontal = 24.dp, vertical = 10.dp),
             ){
             ActionBar()
+            Spacer(modifier=Modifier.height(12.dp))
+            DailyForcast()
+            Spacer(modifier=Modifier.height(16.dp))
+            AirQuality()
         }
 
     }
